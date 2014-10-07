ce59f65efe471df5107fd404d280e7be528ef84c
VeryCB <imcaibin@gmail.com>
2014-10-07 20:25:58 +0800

    test registering commands

--- test_flask.py
+++ test_flask.py
@@ -14,12 +14,39 @@ class App(object):
         self.client = self.app.test_client()
 
 
-@fixture
+@fixture(scope='module')
 def app():
     return App()
 
 
-def test_register_command(app):
+def test_request_without_registering_commands(app):
     res = app.client.get('/')
     assert res.status_code == 200
     assert res.data == b'Command None is not found in team None'
+
+
+def test_registering_commands(app):
+    command = 'sing'
+    token = 'mytoken'
+    team_id = 'MYTEAMID'
+    methods = ['POST']
+    text = 'little apple'
+
+    get_url = '/?token={0}&team_id={1}&command={2}&text={3}'.format(
+        token, team_id, command, text)
+
+    @app.slack.command(command, token, team_id, methods)
+    def _sing_a_song(**kwargs):
+        lyrics = 'You are my littttle apple...'
+        return app.slack.response(lyrics)
+
+    expected_commands = {
+        (team_id, command): (_sing_a_song, token, methods, {}),
+    }
+
+    assert app.slack._commands == expected_commands
+
+    get_res = app.client.get(get_url)
+
+    assert get_res.status_code == 200
+    assert get_res.data == b'GET request is not allowed'
